c717fefeded6b22ef5d238278a98b57e55391015
Andrew Young <yandrew993@gmail.com>
2024-10-13 17:59:29 +0100

    Description of changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,24 +7,26 @@ export default function Home() {
   return (
     <div className="bg-black text-white py-20">
       <div className="container mx-auto text-center">
-        <h1 className="text-center mb-2 tracking-wider">
+        {/* <h1 className="text-center mb-2 tracking-wider">
           THE FASTEST WAY TO GET DEVELOPER RESOURCES
-        </h1>
-        <h1 className="text-4xl font-bold p-4">
+        </h1> */}
+        <h1 className="text-2xl sm:text-3xl md:text-3xl font-semibold sm:font-bold p-4">
           Welcome to your one-stop shop for equipping
-          <br />{" "}
+          <br />
           <span className="tracking-wider">
             web developers with essential resources!
           </span>
         </h1>
-        <p className="pr-80 pl-80 mt-4 text-center mb-10">
+
+        <p className="px-4 sm:px-6 text-center ml-4 md:px-14 lg:px-80 mt-4 text-center mb-10">
           Empower your web development journey with our curated resources.{" "}
           <br />
-          From tutorials to cutting edge tools,our one-stop shop is tailored for
-          all skill level. <br />
-          Elevate your coding expertise and stay ahead inthe dynamic digital
+          From tutorials to cutting edge tools, our one-stop shop is tailored
+          for all skill levels. <br />
+          Elevate your coding expertise and stay ahead in the dynamic digital
           landscape.
         </p>
+
         <button class="bg-gradient-to-r from-orange-400 to-yellow-100 text-black font-semibold py-2 mb-10 px-6 rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300">
           Get started
         </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,84 @@
 "use client"; // This line allows the component to use hooks
 
 import Link from "next/link";
+import { useState } from "react";
+// Add this import
+import { motion } from "framer-motion";
+
+const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Contact us", path: "/contact" },
+  { name: "Resources", path: "/resources" },
+];
 
 export default function Navbar() {
-  const navLinks = [
-    { name: "Home", path: "/" },
-    { name: "About", path: "/about" },
-    { name: "Contact us", path: "/contact" },
-    { name: "Resources", path: "/resources" },
-  ];
+  const [open, setOpen] = useState(false);
+  // Initialize router
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
 
   const isActive = (path) => router.pathname === path;
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-black py-6 shadow-md z-50">
       <div className="container mx-auto flex justify-between items-center mb-4">
-        <div className="flex space-x-6">
+        <div className="hidden md:flex flex space-x-6">
           {navLinks.map((link, index) => (
             <Link key={index} href={link.path}>
               <div className="text-white">{link.name}</div>
             </Link>
           ))}
         </div>
-        <div className="flex space-x-6">
+        <div className="hidden md:flex space-x-6">
           <Link href="/signup">
             <button className="px-3 py-2 bg-black ring-1 ring-white focus:outline-none text-white rounded-full hover:bg-gray-800">
               Signup
@@ -35,8 +91,47 @@ export default function Navbar() {
             </div>
           </Link>
         </div>
+        <div className="md:hidden ml-auto mr-4">
+          <button
+            className="w-10 h-8 flex flex-col justify-between z-50 relative"
+            onClick={() => setOpen(!open)}
+          >
+            <motion.div
+              variants={topVariants}
+              animate={open ? "opened" : "closed"}
+              className="w-10 h-1 bg-white rounded origin-left"
+            ></motion.div>
+            <motion.div
+              variants={centerVariants}
+              animate={open ? "opened" : "closed"}
+              className="w-10 h-1 bg-white rounded"
+            ></motion.div>
+            <motion.div
+              variants={bottomVariants}
+              animate={open ? "opened" : "closed"}
+              className="w-10 h-1 bg-white rounded origin-left"
+            ></motion.div>
+          </button>
+          {open && (
+            <motion.div
+              variants={listVariants}
+              initial="closed"
+              animate="opened"
+              className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+            >
+              {navLinks.map((link) => (
+                <motion.div
+                  variants={listItemVariants}
+                  className=""
+                  key={link.name} // Fixed key and property names
+                >
+                  <Link href={link.path}>{link.name}</Link>
+                </motion.div>
+              ))}
+            </motion.div>
+          )}
+        </div>
       </div>
-
       <hr className="border-1 border-gray-800" />
     </nav>
   );
